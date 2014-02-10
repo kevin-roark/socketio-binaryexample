99e3625188cd3c9ba39596f2b6e364fbc7a3e400
Kevin Roark <ker2143@columbia.edu>
2014-02-10 03:49:00 -0500

    added example to show direct File support

--- app.js
+++ app.js
@@ -12,20 +12,29 @@ var doge_texts = ['plz', 'lol', 'such node',
                   'wow', 'so image', 'am doge',
                   'elite connect'
 ];
+
 function get_doge_text() {
     var items = doge_texts;
     var item = items[Math.floor(Math.random()*items.length)];
     return item;
 }
 
-io.on('connection', function(socket){
-    socket.on('get-doge-plz', function(data) {
-       socket.emit('plz', get_doge_text());
-    });
+io.on('connection', function(socket) {
 
+    // immediately on connection send the client a doge image!!
     fs.readFile(__dirname + '/images/doge.jpg', function(err, data){
         if (err) return done(err);
         socket.emit('doge', data);
+    });
+
+    // when the client asks for doge text, we give it to them (as binary just to show it off)
+    socket.on('get-doge-plz', function(data) {
+       socket.emit('plz', new Buffer(get_doge_text(), 'utf8'));
+    });
+
+    // when the client sends a file, log it here just to see that we get it
+    socket.on('send-file', function(buf) {
+        console.log(buf.toString('utf8'));
     });
 });
 
